c01939afa96ec0986aaf9b0d88c0b8511589f45f
Braulio Sespede <vans_graf@hotmail.com>
2012-10-21 16:35:41 -0300

    calendario + validaciones busqueda

---
+++ project/code/js/date-range.js
@@ -0,0 +1,19 @@
+$(function() {
+        $( "#departure_date" ).datepicker({
+            defaultDate: "+1w",
+            changeMonth: true,
+            numberOfMonths: 3,
+            onSelect: function( selectedDate ) {
+                $( "#return_date" ).datepicker( "option", "minDate", selectedDate );
+            }
+        });
+        $( "#return_date" ).datepicker({
+            defaultDate: "+1w",
+            changeMonth: true,
+            numberOfMonths: 3,
+            onSelect: function( selectedDate ) {
+                $( "#departure_date" ).datepicker( "option", "maxDate", selectedDate );
+            }
+        });
+        $.datepicker.setDefaults($.datepicker.regional['es']);
+    });
---
+++ project/code/js/validate-search.js
@@ -0,0 +1,39 @@
+$(document).ready(function() {
+	$("#search_form").validate({
+        rules:{
+            origin: "required",
+            destination: "required",
+            departure_date: {
+				date: true,
+				required: true
+			},
+			return_date: {
+				date:true,
+				required: true
+			}
+        },
+        messages : {
+            origin: "Lugar de origin erroneo",
+            destination: "Lugar de destino erroneo",
+            departure_date: "Fecha de partida erronea",
+            return_date: "Fecha de regreso erronea"
+        },
+		errorPlacement: function(error, element) { 
+			error.appendTo ( element.next() );
+		}
+    });
+});
+
+$(document).ready(function() {
+		$("#search_button").click(function () {	
+			if (!$("#search_form").valid())
+                alert("Validation  not succeded!");
+			else
+				alert("Validation success!");
+        });
+});
+
+$(document).ready(function() {
+	$("#departure_date").prop("readonly",true);
+	$("#return_date").prop("readonly",true);
+});
